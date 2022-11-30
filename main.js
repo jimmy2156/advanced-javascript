610f2dbba4e5744e114178f53272b37764440036
Gaganjot singh <gaganjot25@gmail.com>
2022-11-30 14:42:59 +0530

    day4

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 // factory functions
-function createCircle(radius) {
-    const circle = {
-        radius: radius,
-        draw() {
-            console.log("draw")
-        }
-    }
-    return circle
-}
-const circle1 = createCircle(2)
-//console.log(circle1.draw())
-function Createcircle(radius) {
-    this.radius = radius,
-    this.draw = function() {
-        console.log("draw")
-    }
+// function createCircle(radius) {
+//     const circle = {
+//         radius: radius,
+//         draw() {
+//             console.log("draw")
+//         }
+//     }
+//     return circle
+// }
+// const circle1 = createCircle(2)
+// //console.log(circle1.draw())
+// function Createcircle(radius) {
+//     this.radius = radius,
+//     this.draw = function() {
+//         console.log("draw")
+//     }
    
-}
-const circle2 = new Createcircle(2)
+// }
+// const circle2 = new Createcircle(2)
 // for (let key in circle2) {
 //     console.log(circle2[key] + 2)
 // }
@@ -76,8 +76,36 @@ const circle2 = new Createcircle(2)
 // console.log(circle2)
 // console.log(areEqual(circle1, circle3))
 // console.log(Object.values(circle3))
-const numbers = [];
-numbers.push(3,4)
-numbers.unshift(1,2)
-numbers.splice(2, 0, 3)
-console.log(numbers.includes(2,3))
+// const numbers = [];
+// numbers.push(3,4)
+// numbers.unshift(1,2)
+// numbers.splice(2, 0, 3)
+// // console.log(numbers.includes(2,3))
+// const courses = [{id: 1, name: "course1"}, {id: 2, name: "course2"}]
+//console.log(courses.find(john => john.name === "course2"))
+// const courses2 = [{id: 10, name: "course6"}, {id: 5, name: "course5"}]
+// const allCourses = [...courses, ...courses2]
+//console.log(allCourses.find(course => console.log(`The count is ${course.id * 8}`)))
+//console.log(allCourses)
+// let sum = ""
+// for (let course of allCourses) {
+//     sum = `${course.name}`
+// }
+// console.log(sum)
+//const numbers = [1,2,3,4];
+// function arrayFromRange(min, max) {
+//     let numbers = []
+//    for (let i = 0; i < max; i++) {
+//     numbers.push(i)
+//    }
+//     return numbers
+// }
+// const numbers = [1,2,3,4]
+// console.log(numbers)
+// function includes(array, number1) {
+//     for (let number of array) 
+//         if (number === number1)
+//           return true
+//         return false
+//  }
+// console.log(includes(numbers, 5))
